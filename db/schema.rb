c8c905cdd990ccdec80b39c7caaf27d595ebdadd
Primov Ulan <ulan.primov@gmail.com>
2019-01-28 19:00:12 +0100

    added faq page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_110724) do
+ActiveRecord::Schema.define(version: 2019_01_28_175419) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2019_01_17_110724) do
     t.string "title"
     t.text "desc"
     t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "faqs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
