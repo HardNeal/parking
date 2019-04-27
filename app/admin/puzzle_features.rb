5ca51c969085a159025a9358abbf33c5b717f5dc
Primov Ulan <ulan.primov@gmail.com>
2019-04-27 11:16:37 +0200

    update

--- app/admin/puzzle_features.rb
+++ app/admin/puzzle_features.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register PuzzleFeature do
-menu parent: 'Управление паззл'
+menu parent: 'Управление Rotary'
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
